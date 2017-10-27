d36d5a63577b3f980bd8ea2e7c55849a37202171
TV <trungvuh@gmail.com>
2017-10-26 22:52:26 -0700

    Finish HW

---
+++ w4d5/NinetyNineCats/app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "everybody@appacademy.io"
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/session/new'
+    mail(to: user.username, subject: 'Welcome to 99 Cats')
+  end
+end
