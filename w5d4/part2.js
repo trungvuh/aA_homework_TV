bafb419264a324feb5d72a45994551c14ac9a586
TV <trungvuh@gmail.com>
2017-11-02 09:14:46 -0700

    HW

---
+++ w5d4/part2.js
@@ -0,0 +1,32 @@
+function titleize(array, callback) {
+  let result = array.map(el => `Mx. ${el} Jingleheimer Schmidt`);
+  callback(result);
+};
+
+titleize(["Mary", "Brian", "Leo"], (names) => {
+  names.forEach(name => console.log(name));
+});
+
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  trickIndex = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+};
